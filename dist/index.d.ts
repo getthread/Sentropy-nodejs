3d913e0c847e1b5596d05185bb05ce0c4d80ebe9
Steven Layne <stevenolayne@gmail.com>
2019-08-22 17:54:18 -0400

    removed dist

--- dist/index.d.ts
+++
@@ -1,6 +0,0 @@
-import { SentropyClient } from "./sentropy";
-export * from './sentropy';
-declare function sentropy(apiKey: string, headers?: {
-    [key: string]: string;
-}): SentropyClient;
-export default sentropy;
