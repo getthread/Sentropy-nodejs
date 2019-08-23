6cdbabb6640750d12d2730440f73dc9539e315b4
Steven Layne <stevenolayne@gmail.com>
2019-08-23 10:33:02 -0400

    moved types to types file

---
+++ src/types.ts
@@ -0,0 +1,22 @@
+export type ClassificationRequestPayload = {
+  id?: string;
+  text: string;
+  author: string;
+  segment: string;
+};
+
+export type SentropyClientOpts = {
+  headers?: { [key: string]: string };
+};
+
+export type ClassificationResponsePayload = {
+  id: string;
+  author: string;
+  segment: string;
+  label_probabilities: {
+    IDENTITY_ATTACK: number;
+    SEXUAL_AGGRESSION: number;
+    PHYSICAL_VIOLENCE: number;
+    NEONAZISM: number;
+  };
+};
